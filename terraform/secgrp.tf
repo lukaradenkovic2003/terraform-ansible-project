745e093b14a1e241feb239b8e9ead62c51292103
lukaradenkovic2003 <lukaradenkovic@libero.it>
2026-07-20 20:53:57 +0200

    Added secgrp for EC2 and ingress and egress rules

--- terraform/secgrp.tf
+++ terraform/secgrp.tf
@@ -30,3 +30,28 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_fromALB" {
   ip_protocol       = "-1"
 }
 
+resource "aws_security_group" "devops-assessment-ec2-sg" {
+  name        = "devops-assessment-ec2-sg"
+  description = "Security group for ec2"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name      = "devops-assessment-ec2"
+    ManagedBy = "Terraform"
+    Project   = "DevOps assessment"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_http_fromALB" {
+  security_group_id            = aws_security_group.devops-assessment-ec2-sg.id
+  referenced_security_group_id = aws_security_group.devops-assessment-alb-sg.id
+  ip_protocol                  = "tcp"
+  from_port                    = 80
+  to_port                      = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_fromEC2" {
+  security_group_id = aws_security_group.devops-assessment-ec2-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
