aad01a3ba3055c3832192f63f0529ec4ecd2a1af
lukaradenkovic2003 <lukaradenkovic@libero.it>
2026-07-20 20:27:57 +0200

    Added azs , subnets and rules for gateway, dns , ip ,db subnet rt and vars in varibles.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,54 @@
 variable "aws_region" {
-  description = "AWS region gde se postavlja infrastruktura"
+  description = "AWS region"
   type        = string
-  default     = "eu-east-1"
+  default     = "us-east-1"
+}
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+variable "PROJECT" {
+  default = "DevOps assessment"
+}
+variable "Zone1" {
+  default = "us-east-1a"
+}
+variable "Zone2" {
+  default = "us-east-1b"
+}
+variable "Zone3" {
+  default = "us-east-1c"
+}
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
+}
+
+variable "DbSub1CIDR" {
+  default = "172.21.7.0/24"
+}
+variable "DbSub2CIDR" {
+  default = "172.21.8.0/24"
+}
+variable "DbSub3CIDR" {
+  default = "172.21.9.0/24"
 }
---
+++ terraform/vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.VPC_NAME
+  cidr = var.VpcCIDR
+
+  azs             = [var.Zone1, var.Zone2, var.Zone3]
+  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
+  public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
+database_subnets = [var.DbSub1CIDR, var.DbSub2CIDR, var.DbSub3CIDR]
+
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
+create_database_subnet_route_table = true
+
+  tags = {
+    Name    = var.VPC_NAME
+    Project = var.PROJECT
+  }
+}
